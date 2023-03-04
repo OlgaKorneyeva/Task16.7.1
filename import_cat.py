4d42dbedef5f3a5c2bd8d6adb6f2de0fa9bbd24e
unknown <olga.korneyeva@bk.ru>
2023-03-04 08:36:57 +0200

    All files is added

---
+++ import_cat.py
@@ -0,0 +1,9 @@
+from class_Cat import Cat
+
+Baron = Cat("Барон", "мальчик", 2)
+
+Sam = Cat("Сэм", "мальчик", 2)
+
+print(f'имя: {Baron.get_name()}, пол: {Baron.get_gender()}, возраст: {Baron.get_age()}')
+print(f'имя: {Sam.get_name()}, пол: {Sam.get_gender()}, возраст: {Sam.get_age()}')
+
